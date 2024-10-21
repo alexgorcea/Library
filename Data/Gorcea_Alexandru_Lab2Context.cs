7b24807b26e778940a03ce1a014a8e2100a4cb49
Alex Gorcea <alexandru.gorcea1@stud.ubbcluj.ro>
2024-10-21 16:46:26 +0300

    Add project files.

---
+++ Data/Gorcea_Alexandru_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Gorcea_Alexandru_Lab2.Models;
+
+namespace Gorcea_Alexandru_Lab2.Data
+{
+    public class Gorcea_Alexandru_Lab2Context : DbContext
+    {
+        public Gorcea_Alexandru_Lab2Context (DbContextOptions<Gorcea_Alexandru_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Gorcea_Alexandru_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<Gorcea_Alexandru_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<Gorcea_Alexandru_Lab2.Models.Author> Author { get; set; } = default!;
+    }
+}
---
+++ Migrations/20241021134313_Authors.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gorcea_Alexandru_Lab2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Authors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Publisher_PublisherId",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "Author",
+                table: "Book");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Publisher",
+                newName: "ID");
+
+            migrationBuilder.RenameColumn(
+                name: "PublisherId",
+                table: "Book",
+                newName: "PublisherID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Book_PublisherId",
+                table: "Book",
+                newName: "IX_Book_PublisherID");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AuthorID",
+                table: "Book",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Author",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Author", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Book_AuthorID",
+                table: "Book",
+                column: "AuthorID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Author_AuthorID",
+                table: "Book",
+                column: "AuthorID",
+                principalTable: "Author",
+                principalColumn: "ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Publisher_PublisherID",
+                table: "Book",
+                column: "PublisherID",
+                principalTable: "Publisher",
+                principalColumn: "ID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Author_AuthorID",
+                table: "Book");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Book_Publisher_PublisherID",
+                table: "Book");
+
+            migrationBuilder.DropTable(
+                name: "Author");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Book_AuthorID",
+                table: "Book");
+
+            migrationBuilder.DropColumn(
+                name: "AuthorID",
+                table: "Book");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Publisher",
+                newName: "Id");
+
+            migrationBuilder.RenameColumn(
+                name: "PublisherID",
+                table: "Book",
+                newName: "PublisherId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Book_PublisherID",
+                table: "Book",
+                newName: "IX_Book_PublisherId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Author",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Book_Publisher_PublisherId",
+                table: "Book",
+                column: "PublisherId",
+                principalTable: "Publisher",
+                principalColumn: "Id");
+        }
+    }
+}
