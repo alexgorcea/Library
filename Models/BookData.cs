16ffe2dca52c9b7cf19ccbfcf0e695e9d3352577
Alex Gorcea <alexandru.gorcea1@stud.ubbcluj.ro>
2024-10-25 15:31:43 +0300

    Categories Checkboxs

---
+++ Models/BookData.cs
@@ -0,0 +1,9 @@
+﻿namespace Gorcea_Alexandru_Lab2.Models
+{
+    public class BookData
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<Book> Categories { get; set; }
+        public IEnumerable<BookCategory> BookCategories { get; set; }
+    }
+}
--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -10,7 +10,7 @@ using Gorcea_Alexandru_Lab2.Models;
 
 namespace Gorcea_Alexandru_Lab2.Pages.Books
 {
-    public class CreateModel : PageModel
+    public class CreateModel : BookCategoriesPageModel
     {
         private readonly Gorcea_Alexandru_Lab2.Data.Gorcea_Alexandru_Lab2Context _context;
 
@@ -21,8 +21,18 @@ namespace Gorcea_Alexandru_Lab2.Pages.Books
 
         public IActionResult OnGet()
         {
+            var authorList = _context.Author.Select(x => new
+            {
+                x.ID,
+                FullName = x.LastName + " " + x.FirstName
+            });
+
             ViewData["PublisherID"] = new SelectList(_context.Set<Publisher>(), "ID", "PublisherName");
-            ViewData["AuthorID"] = new SelectList(_context.Set<Author>(), "ID", "LastName");
+            ViewData["AuthorID"] = new SelectList(authorList, "ID", "FullName");
+
+            var book = new Book();
+            book.BookCategories = new List<BookCategory>();
+            PopulateAssignedCategoryData(_context, book);
 
             return Page();
         }
@@ -31,13 +41,22 @@ namespace Gorcea_Alexandru_Lab2.Pages.Books
         public Book Book { get; set; } = default!;
 
         // For more information, see https://aka.ms/RazorPagesCRUD.
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string[] selectedCategories)
         {
-            if (!ModelState.IsValid)
+            var newBook = new Book();
+            if (selectedCategories != null)
             {
-                return Page();
+                newBook.BookCategories = new List<BookCategory>();
+                foreach (var cat in selectedCategories)
+                {
+                    var catToAdd = new BookCategory
+                    {
+                        CategoryID = int.Parse(cat)
+                    };
+                    newBook.BookCategories.Add(catToAdd);
+                }
             }
-
+            Book.BookCategories = newBook.BookCategories;
             _context.Book.Add(Book);
             await _context.SaveChangesAsync();
 
