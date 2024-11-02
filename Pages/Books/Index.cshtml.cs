2ea610fa1f3cd10c3b7bcad3336c61bd0b02e112
Alex Gorcea <alexandru.gorcea1@stud.ubbcluj.ro>
2024-11-02 15:51:45 +0200

    Sorting books by book's name % author"s name

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -23,10 +23,16 @@ namespace Gorcea_Alexandru_Lab2.Pages.Books
         public BookData BookD { get; set; }
         public int BookID { get; set; }
         public int CategoryID { get; set; }
-        public async Task OnGetAsync(int? id, int? categoryID)
+
+        public string TitleSort { get; set; }
+        public string AuthorSort { get; set; }
+        public async Task OnGetAsync(int? id, int? categoryID, string sortOrder)
         {
             BookD = new BookData();
- 
+
+            TitleSort = String.IsNullOrEmpty(sortOrder) ? "title_desc" : "";
+            AuthorSort = sortOrder == "author" ? "author_desc" : "author";
+
             BookD.Books = await _context.Book
             .Include(b => b.Author)
             .Include(b => b.Publisher)
@@ -41,6 +47,26 @@ namespace Gorcea_Alexandru_Lab2.Pages.Books
                 Book book = BookD.Books
                 .Where(i => i.ID == id.Value).Single();
                 BookD.Categories = (IEnumerable<Book>)book.BookCategories.Select(s => s.Category);
+            }
+
+            switch (sortOrder)
+            {
+                case "title_desc":
+                    BookD.Books = BookD.Books.OrderByDescending(s => s.Title);
+                    break;
+
+                case "author_desc":
+                    BookD.Books = BookD.Books.OrderByDescending(s => s.Author.FullName);
+                    break;
+
+                case "author":
+                    BookD.Books = BookD.Books.OrderBy(s => s.Author.FullName);
+                    break;
+
+                default:
+                    BookD.Books = BookD.Books.OrderBy(s => s.Title);
+                    break;
+
             }
         }
     }
