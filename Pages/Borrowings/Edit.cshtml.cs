4920ece9250d5560454b203869112c786b65b30d
Alex Gorcea <alexandru.gorcea1@stud.ubbcluj.ro>
2024-11-11 20:26:27 +0200

    Tema Lab5

--- Pages/Borrowings/Edit.cshtml.cs
+++ Pages/Borrowings/Edit.cshtml.cs
@@ -30,14 +30,18 @@ namespace Gorcea_Alexandru_Lab2.Pages.Borrowings
                 return NotFound();
             }
 
-            var borrowing =  await _context.Borrowing.FirstOrDefaultAsync(m => m.ID == id);
+            var borrowing =  await _context.Borrowing
+                 .Include(b => b.Member)
+                .Include(b => b.Book)
+                .FirstOrDefaultAsync(m => m.ID == id);
             if (borrowing == null)
             {
                 return NotFound();
             }
             Borrowing = borrowing;
-           ViewData["BookID"] = new SelectList(_context.Book, "ID", "ID");
-           ViewData["MemberID"] = new SelectList(_context.Set<Member>(), "ID", "ID");
+
+            ViewData["BookID"] = new SelectList(_context.Book, "ID", "Title");
+            ViewData["MemberID"] = new SelectList(_context.Member, "ID", "FullName");
             return Page();
         }
 
